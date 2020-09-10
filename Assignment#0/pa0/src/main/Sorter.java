c4da9a129337809c1495ce09932090f6c5ce9dab
whoareyouwhoami <young.woo@hotmail.com>
2020-09-10 10:53:42 +0900

    Assignmnet 0 final

--- Assignment#0/pa0/src/main/Sorter.java
+++ Assignment#0/pa0/src/main/Sorter.java
@@ -1,7 +1,7 @@
 
 /*
- * Name: 
- * Student ID #: 
+ * Name: Young Woo Ju
+ * Student ID #: 2014122074 
  */
 
 /* 
@@ -25,7 +25,7 @@ public class Sorter implements ISorter {
 		// Compare and merge
 		while(leftIndex < left.length || rightIndex < right.length) {
 			// If left array has reached its last index
-			// Copy the rest of right array
+			// Copy the rest of RIGHT array
 			if(leftIndex == left.length) {
 				for(int r = rightIndex; r < right.length; r++) {
 					sortedArray[sortedIndex] = right[rightIndex];
@@ -34,7 +34,7 @@ public class Sorter implements ISorter {
 				}
 			} 
 			// If right array has reached its last index
-			// Copty the rest of left array
+			// Copty the rest of LEFT array
 			else if(rightIndex == right.length) {
 				for(int l = leftIndex; l < left.length; l++) {
 					sortedArray[sortedIndex] = left[leftIndex];
@@ -42,7 +42,7 @@ public class Sorter implements ISorter {
 					leftIndex++;
 				}
 			} 
-			// Else compare and merge in ascending order
+			// Else compare and merge in ascending/descending order
 			else {
 				if(type=="asc") {
 					if(left[leftIndex] < right[rightIndex]) {
@@ -70,9 +70,12 @@ public class Sorter implements ISorter {
 
 	private int[] splitArray(int[] xArray, int start, int end) {
 		int k = 0;
+		
+		//  Set array size
 		int arraySize = end - start + 1;
 		int[] resultArray = new int[arraySize];
 		
+		// Copying array
 		for(int i = start; i < end + 1; i++) {
 			resultArray[k] = xArray[i];
 			k++;
@@ -81,21 +84,24 @@ public class Sorter implements ISorter {
 	}
 
 	private int[] mergeSort(String type, int[] unorderArray) {
+		// If array is empty 
 		if(unorderArray.length == 0) {
 			return unorderArray;
 		}
 
 		// Find mid point and divide the array into two part]
-		int midIndex = unorderArray.length/2;
+		int midIndex = unorderArray.length / 2;
 
 		// Splitting into two arrays
-		int[] splittedLeft = splitArray(unorderArray, 0, midIndex-1);
-		int[] splittedRight = splitArray(unorderArray, midIndex, unorderArray.length-1);
+		int[] splittedLeft = splitArray(unorderArray, 0, midIndex - 1);
+		int[] splittedRight = splitArray(unorderArray, midIndex, unorderArray.length - 1);
 		
+		// When there is no more split return the array
 		if(unorderArray.length == 1) {
 			return unorderArray;
 		}
 
+		// Recursion + Merging
 		int[] l = mergeSort(type, splittedLeft);
 		int[] r = mergeSort(type, splittedRight);
 		int[] res = mergeSortedArray(type, l,r);
--- Assignment#0/pa0/src/test/SorterTests.java
+++ Assignment#0/pa0/src/test/SorterTests.java
@@ -30,4 +30,37 @@ public class SorterTests {
 			assertThat(s.descending(a.clone()), is(new int[] { 4, 3, 2, 1 }));
 		});
 	}
+
+	@Test
+	@Score(1)
+	public void testBasic3() {
+		assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
+			ISorter s = new Sorter();
+			int[] a = new int[] { 1, 3, 1, 5, 2, 10, 11, 13, 10, 11 };
+			assertThat(s.ascending(a.clone()), is(new int[] { 1, 1, 2, 3, 5, 10, 10, 11, 11, 13 }));
+			assertThat(s.descending(a.clone()), is(new int[] { 13, 11, 11, 10, 10, 5, 3, 2, 1, 1 }));
+		});
+	}
+
+	@Test
+	@Score(1)
+	public void testBasic4() {
+		assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
+			ISorter s = new Sorter();
+			int[] a = new int[] { 100, 0, 1, 3, 6, 3, 13, 18, 2, 1 };
+			assertThat(s.ascending(a.clone()), is(new int[] { 0, 1, 1, 2, 3, 3, 6, 13, 18, 100 }));
+			assertThat(s.descending(a.clone()), is(new int[] { 100, 18, 13, 6, 3, 3, 2, 1, 1, 0 }));
+		});
+	}
+
+	@Test
+	@Score(1)
+	public void testBasic5() {
+		assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
+			ISorter s = new Sorter();
+			int[] a = new int[] { 5, -1, -3, 2 , 0 , 1 };
+			assertThat(s.ascending(a.clone()), is(new int[] { -3, -1, 0, 1, 2, 5 }));
+			assertThat(s.descending(a.clone()), is(new int[] { 5, 2, 1, 0, -1, -3 }));
+		});
+	}
 }
